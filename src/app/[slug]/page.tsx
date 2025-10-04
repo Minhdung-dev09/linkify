47ccc9340459de7949eca55de8944db1eb551322
BunsThuis <2121050986@student.humg.edu.vn>
2025-10-05 01:14:17 +0700

    main

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -16,7 +16,7 @@ export default function LandingPageViewer() {
     const fetchLandingPage = async () => {
       try {
         setLoading(true);
-        const response = await fetch(`http://localhost:5000/${slug}`, {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${slug}`, {
           method: 'GET',
           headers: {
             'Accept': 'text/html',
